00ab9a2eb23b81a591a2026afc7a8e4c4b37c0c3
Jaroslaw Cimoch <jaroslaw.cimoch@aspiresys.com>
2019-06-28 09:23:56 +0200

    fix type

--- workshops-workspace/apps/nasa/src/app/core/services/app-config.service.ts
+++ workshops-workspace/apps/nasa/src/app/core/services/app-config.service.ts
@@ -1,21 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+export interface AppConfig {
+    config: { isGodModeActive: boolean }
+}
+
 @Injectable()
 export class AppConfigService {
-  private appConfig: {config: {isGodModeActive: boolean}};
+    private appConfig: AppConfig;
 
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) {
+    }
 
-  loadAppConfig() {
-    return this.http.get('/assets/data/app-config.json')
-      .toPromise()
-      .then(data => {
-        this.appConfig = data;
-      });
-  }
+    loadAppConfig() {
+        return this.http.get('/assets/data/app-config.json')
+            .toPromise()
+            .then((data: AppConfig) => {
+                this.appConfig = data;
+            });
+    }
 
-  getConfig() {
-    return this.appConfig;
-  }
+    getConfig() {
+        return this.appConfig;
+    }
 }
